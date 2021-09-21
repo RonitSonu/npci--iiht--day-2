718fadc57775c0f7b39bedb58babfcc91524211c
Ronit Sahu <90675443+RonitSonu@users.noreply.github.com>
2021-09-21 16:44:13 +0530

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+class A4{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		reverseFibonacci(n);
+	
+	}
+	
+
+	static void reverseFibonacci(int n)
+	{
+		int a[] = new int[n];
+	
+		a[0] = 0;
+		a[1] = 1;
+	
+		for (int i = 2; i < n; i++)
+		{
+
+			a[i] = a[i - 2] + a[i - 1];
+		}
+	
+		for (int i = n - 1; i >= 0; i--)
+		{
+
+			System.out.print(a[i] +" ");
+		}
+	}
+	
+	
+}
+
+
